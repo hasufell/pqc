ece632b5c2b40d89bad71084989cf42d85c422be
hasufell <hasufell@hasufell.de>
2014-04-20 19:50:49 +0200

    MEM: introduce use our own ntru_malloc() function
    Use this instead of malloc().

---
+++ src/mem.c
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2014 FH Bielefeld
+ *
+ * This file is part of a FH Bielefeld project.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301  USA
+ */
+
+#include "mem.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * Allocate memory of size and return
+ * a void pointer.
+ *
+ * @param size of the memory to allocate in bytes
+ * @return void pointer to the beginning of the allocated memory block
+ */
+void *ntru_malloc(size_t size)
+{
+	void *ptr;
+
+	ptr = malloc(size);
+
+	if (size)
+		if (!ptr) {
+			fprintf(stderr, "failed to allocate memory, aborting!");
+			abort();
+		}
+
+	return ptr;
+}
